fbde232e63aa0c9c6e901f1db66d5850eb481be6
kmzr9b67 <kmzr9b67@gmail.com>
2024-05-09 22:16:19 +0200

    Improving code naming.

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
 import requests
 import smtplib
 from creds import *
-
 URL_WEB = 'https://waw-cinema-assistant-mtfo73rvoa-lm.a.run.app/final'
 
 
-def html(url):
+def make_request(url):
     response = requests.get(url)
     return response.status_code
 
 
-def send_mail(status):
+def send_mail(response_code):
     with smtplib.SMTP('smtp.gmail.com') as connection:
         connection.starttls()
         connection.login(user=SENDER, password=PASSWORD)
         connection.sendmail(from_addr=SENDER,to_addrs=ADDRESS,
-                            msg=f"In waw-cinema-assistant there is an error number {status}")
+                            msg=f"In waw-cinema-assistant there is an error number {response_code}")
 
 
-def what_does_code_mean(url):
-    respond = html(url)
-    if respond != 200:
-        send_mail(respond)
+def if_problem_send_mail(url):
+    response_code = make_request(url)
+    if response_code != 200:
+        send_mail(response_code)
     else:
         pass
 
 
-what_does_code_mean(URL_WEB)
-
+if_problem_send_mail(URL_WEB)
